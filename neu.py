03a5411eca9cb69e7ca592c9959d510e9c5c582a
Laura Koepf <laura.koepf@cavorit.de>
2023-11-28 15:27:23 +0100

    labeling indexes/rows and columns

--- neu.py
+++ neu.py
@@ -4,9 +4,12 @@ import pandas as pd
 
 random_data = np.random.randint(1 , 101, [3, 5])
 
+row_labels = ["ROW 1", "ROW 2", "ROW 3"]
 
-cities = pd.DataFrame(data = random_data)
+coloumn_labels = ("COLUMN 1", "COLUMN 2", "COLUMN 3", "COLUMN 4", "COLUMN 5")
+
+data_frame_test = pd.DataFrame(data = random_data, index = row_labels, columns = coloumn_labels)
 
 
-print(cities)
+print(data_frame_test)
 
