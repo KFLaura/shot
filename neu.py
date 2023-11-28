9ea3cdc487301ea0204d760a1720f4c99d9083f0
Laura Koepf <laura.koepf@cavorit.de>
2023-11-28 13:10:42 +0100

    First basic pandas DataFrame

--- neu.py
+++ neu.py
@@ -1,5 +1,14 @@
+import numpy as np 
+import pandas as pd 
 
-import sys
-print(sys.path)
-print("hello world")
 
+city_data = {
+    "City": ["NYC", "Paris", "Rome"],
+    "Country": ["US", "France", "Italy"],
+    "Population": [8600000, 2141000, 2873000]
+}
+
+
+cities = pd.DataFrame(city_data)
+
+print(cities)
