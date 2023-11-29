e7485ab2329cb8804bdb980c1ca6d4b81f26eefc
KFLaura <151622768+KFLaura@users.noreply.github.com>
2023-11-29 09:56:40 +0100

    plot_hole_circle.py

--- plot_hole_circle.py
+++ plot_hole_circle.py
@@ -9,12 +9,16 @@ random_shot = 30
 random_x = np.random.uniform(-1.0,1.0, random_shot)
 random_y = np.random.uniform(-1.0,1.0, random_shot)
 
+
+
 # init empty containers
 red_out_of_circle_x = []
 red_out_of_circle_y = []
 
 green_within_or_on_circle_x = []
 green_within_or_on_circle_y = []
+
+
 
 # compute
 a  = np.array((0,0))
@@ -34,9 +38,9 @@ for i in range(random_shot):
         green_within_or_on_circle_x.insert(0, random_x[i])
         green_within_or_on_circle_y.insert(0, random_y[i])
         
-    
 
-#y-value of circleline 
+
+#y-value of circleline - upper half 
 y_value_1 = []
 
 for i in range(-100, 101):
@@ -49,6 +53,8 @@ for i in range(-100, 101):
     y_value_1.append(y)
 
 
+
+#y-value of circleline - lower half
 y_value_2 = []
 for i in range(-100, 101):
     x = i/100
@@ -60,13 +66,15 @@ for i in range(-100, 101):
 
 
 
-#x-value of circleline
+#x-value of circleline - upper half
 x_value_1 = []
 for i in range(-100, 101):
     x = i/100
     x_value_1.append(x)
 
 
+
+#x-value of circleline - lower half
 x_value_2 = []
 for i in range(-100, 101):
     x = i/100
@@ -86,15 +94,19 @@ plt.ylim(-1.0, 1.0)
 ax.set_aspect("equal", adjustable="box")
 
 
+
 #create scatter
 plt.scatter(red_out_of_circle_x, red_out_of_circle_y, c = 'red', s = 8)
 plt.scatter(green_within_or_on_circle_x, green_within_or_on_circle_y, c = 'green', s = 8)
 
 
-#create circleline  
+
+#plot circleline  
 plt.plot(x_value_1, y_value_1, color = 'green',  linewidth = 4)
 
 plt.plot(x_value_2, y_value_2, color = 'green',  linewidth = 4)
+
+
 
 
 #create area filling 
@@ -110,6 +122,9 @@ filling_y_2 = y_value_2
 filling_inside = plt.fill_between(filling_x_2, filling_y_2, color = 'green', alpha = 0.15)
 filling_ouside = plt.fill_between(x_value_2, y_value_2, np.max(-1), color = 'red', alpha = 0.15)
 
+
+
+
 # create label of the total number off shots 
 plt.title("Anzahl Schüsse insg.: " + str(random_shot), family="serif", color="black", weight="normal", size=14)
 
@@ -120,10 +135,10 @@ percent_hits = (len(green_within_or_on_circle_x)*100)/random_shot
 percent_hits_rounded = round(percent_hits, 2)
 
 
+
 #compute probability of loost shots in percent roundet to second decimal place 
 percent_lost = (len(red_out_of_circle_x)*100)/random_shot
 percent_lost_rounded = round(percent_lost, 2)
-
 
 
 
